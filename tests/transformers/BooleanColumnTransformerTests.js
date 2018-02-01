1a2d8a1dd054b6554faced31c5cce152b488ee0d
Josh <josh@phin.co>
2018-01-31 21:27:41 -0800

    boolean transformer

--- tests/transformers/BooleanColumnTransformerTests.js
+++ tests/transformers/BooleanColumnTransformerTests.js
@@ -4,19 +4,30 @@ const assert = require('chai').assert;
 
 describe( 'BooleanColumnTransformer', function(){
 
-	it( 'works', async function(){
+	it( 'works with truthy values', async function(){
 		const transformer = new BooleanColumnTransformer( {}, "source" );
 		assert.isTrue( transformer.transform('yes') );
+		assert.isTrue( transformer.transform(' yes  ') );
+		assert.isTrue( transformer.transform(' YeS  ') );
+		assert.isTrue( transformer.transform(' trUe  ') );
 		assert.isTrue( transformer.transform('TRUE') );
+		assert.isTrue( transformer.transform(' 1 ') );
 		assert.isTrue( transformer.transform( 1 ) );
+		assert.isTrue( transformer.transform( 1.0 ) );
 		assert.isTrue( transformer.transform( true ) );
-		
+	});
+	
+	it( 'works with falsey values', async function(){
+		const transformer = new BooleanColumnTransformer( {}, "source" );
 		assert.isFalse( transformer.transform( null ) );
 		assert.isFalse( transformer.transform( void 0 ) );
+		assert.isFalse( transformer.transform( NaN ) );
 		assert.isFalse( transformer.transform( 'false' ) );
+		assert.isFalse( transformer.transform( '  False ' ) );
 		assert.isFalse( transformer.transform( 'NO' ) );
+		assert.isFalse( transformer.transform( '0' ) );
 		assert.isFalse( transformer.transform( 0 ) );
 		assert.isFalse( transformer.transform( false ) );
 	});
-
+	
 });
